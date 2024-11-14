4b9f902fb13fa90e7a2a2c81e8ca9f0344c79860
TandiweBeta <tandiwebeta@gmail.com>
2024-11-14 23:21:30 +0200

    Added two more files

--- my/src/Dictionary.js
+++ my/src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, {useState} from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results.js";
 
 export default function Dictionary(){
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-   console.log(response.data);
+   console.log(response.data[0]);
+   setResults(response.data[0])
   }
  
   
@@ -17,19 +20,19 @@ export default function Dictionary(){
 
     let apiKey = "00c99d472fbddtac3e3b2d017a4afdbo";
     let apiUrl =
-    `https://api.shecodes.io/dictionary/v1/define?${keyword}&key=${apiKey}`;
+    `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
       axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event){
     setKeyword(event.target.value);
 
   }
-  return( 
-    <div className= "Dictionary">
+  return (
+    <div className="Dictionary">
       <form onSubmit={search}>
-       <input type="search" onChange= {handleKeywordChange}/>
-
-    </form>
-  </div>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
+    </div>
   );
 }
